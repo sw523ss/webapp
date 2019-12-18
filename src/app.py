2bf36f2679acc3238652d4f09670f5802e553c1d
STEVE <sw523ss@njit.edu>
2019-12-18 01:01:11 -0500

    Feature: Register user_api blueprint in app Task: Added code to app.py

--- src/app.py
+++ src/app.py
@@ -6,6 +6,8 @@ from flask import Flask
 from .config import app_config
 from .models import db, bcrypt
 
+#import user_api blueprint
+from .views.UserView import user_api as user_blueprint
 
 def create_app(env_name):
     """
@@ -22,6 +24,8 @@ def create_app(env_name):
     bcrypt.init_app(app)
 
     db.init_app(app)
+
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
 
     @app.route('/', methods=['GET'])
     def index():
