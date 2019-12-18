bbdb5860512160abb519c525d2796263367a43e8
STEVE <sw523ss@njit.edu>
2019-12-18 00:46:21 -0500

    Feature: Wrap db and bcrypt with app Task: Added code to /src/app.py

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@
 from flask import Flask
 
 from .config import app_config
+from .models import db, bcrypt
 
 
 def create_app(env_name):
@@ -15,6 +16,12 @@ def create_app(env_name):
     app = Flask(__name__)
 
     app.config.from_object(app_config[env_name])
+
+    # initializing bcrypt
+
+    bcrypt.init_app(app) 
+
+    db.init_app(app)
 
     @app.route('/', methods=['GET'])
     def index():
