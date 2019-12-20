43b920eee6d7c4e4884c94df36a6fdbdb181dc14
STEVE <sw523ss@njit.edu>
2019-12-20 00:12:06 -0500

    Feature: Create a testing configuration in config.py for part 3 Task: added code to config.py

--- src/config.py
+++ src/config.py
@@ -21,7 +21,19 @@ class Production(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
+# create a testing configuration in config.py
+
+class Testing(object):
+    """
+    Development environment configuration
+    """
+    TESTING = True
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
 app_config = {
     'development': Development,
     'production': Production,
+    'testing': Testing
 }
